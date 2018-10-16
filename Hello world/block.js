778d1b02bdbf0e61456145dc266d2b106a302d4c
easwar1213 <easwaran.k@mitosistech.com>
2018-10-16 19:36:37 +0500

    sample

--- Hello world/block.js
+++ Hello world/block.js
@@ -1,26 +1,55 @@
+//Blocking
+// var http = require('http');
+// var url = require('url');
+// var cp = require('child_process');
 
+// function onRequest(request, response) {
+//     var pathname = url.parse(request.url).pathname;
+//     if (pathname == '/wait') {
+//         cp.exec('node block.js', myCallback);
+//     }
+//     else {
+//         response.writeHead(200, { 'Content-Type': 'text/plain' });
+//         response.write('Hello!\n');
+//         response.end();
+//     }
+
+//     console.log('New connection');
+
+//     function myCallback() {
+//         response.writeHead(200, { 'Content-Type': 'text/plain' });
+//         response.write('Thanks for waiting!\n');
+//         response.end();
+//     }
+// }
+// http.createServer(onRequest).listen(8080);
+// console.log('Server started');
+
+
+
+
+
+
+
+///Non-Blocking
 var http = require('http');
-var url = require('url');
-var cp = require('child_process');
+var fileSystem = require('fs');
 
-function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    if (pathname == '/wait') {
-        cp.exec('node block.js', myCallback);
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    var read_stream = fileSystem.createReadStream('myfile.txt');
+    read_stream.on('data', writeCallback);
+    read_stream.on('close', closeCallback);
+
+    function writeCallback(data) {
+        response.write(data);
     }
-    else {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('Hello!\n');
+
+    function closeCallback() {
         response.end();
     }
 
-    console.log('New connection');
+}).listen(8080);
 
-    function myCallback() {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('Thanks for waiting!\n');
-        response.end();
-    }
-}
-http.createServer(onRequest).listen(8080);
 console.log('Server started');
---
+++ filesystem/package.js
@@ -0,0 +1,11 @@
+
+{
+    "name"; "filesystem",
+        "version"; "1.0.0",
+            "description"; "File System Example",
+                "main"; "filesystem",
+                    "author"; "JournalDEV",
+                        "engines"; {
+        "node"; "*"
+    }
+}
